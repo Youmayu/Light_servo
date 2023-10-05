f25f8a57b36d5f14a06929e09b7a253329860d10
Youmayu <goldenmaker2000@gmail.com>
2023-10-05 12:43:04 +0200

    first commit

---
+++ Light_servo/Light_servo.ino
@@ -0,0 +1,15 @@
+#include <Servo.h>
+const int servoPin = 9;
+const int t =100;
+
+Servo servo;
+
+void setup(){
+    servo.attach(servoPin);
+}
+void loop(){
+    servo.write(0);
+    delay(t);
+    servo.write(180);
+    delay(t);
+}
